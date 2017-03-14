92b47248f6354ab085194089ff8ed6f879123239
Jason Luck <jason.luck@icfi.com>
2017-03-14 10:21:44 -0400

    Adding Circle CI build file

--- src/main/java/com/lucksolutions/service/employment/model/VerificationRequest.java
+++ src/main/java/com/lucksolutions/service/employment/model/VerificationRequest.java
@@ -4,18 +4,16 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
+import lombok.Data;
+
 import com.lucksolutions.service.model.AbstractModelObject;
 
-import lombok.Getter;
-import lombok.Setter;
-
+@Data
 public class VerificationRequest extends AbstractModelObject {
 
 
 	private static final long serialVersionUID = -6373869410947994045L;
 	
-	@Getter
-	@Setter
 	@Min(100000000) // 9 Digits
 	@Max(9999999999L) // 10 Digits
 	@NotNull
--- src/main/java/com/lucksolutions/service/model/AbstractModelObject.java
+++ src/main/java/com/lucksolutions/service/model/AbstractModelObject.java
@@ -2,9 +2,12 @@ package com.lucksolutions.service.model;
 
 import java.io.Serializable;
 
+import lombok.Data;
+
 import org.springframework.validation.annotation.Validated;
 
 @Validated
+@Data
 public abstract class AbstractModelObject implements Serializable {
 
 	private static final long serialVersionUID = 1890059803108653170L;
